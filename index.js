01644180c9e23790718a8faa80a30c6d45c0b546
FelipeAnacleto <99516011+FelipeAnacleto@users.noreply.github.com>
2022-09-25 15:58:08 -0300

    commit

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.listen('3000');
 
-app.route('/').get ((req, res) => res.send("<h1>Hello</h1>") )
+// app.route('/').get ((req, res) => res.send("<h1>Hello</h1>") )
 
-app.route('/sobre').get ((req, res) => res.send("<p><strong>Hello sobre</strong></p>") )
+// app.route('/sobre').get ((req, res) => res.send("<p><strong>Hello sobre</strong></p>") )
+
+// app.use(express.json());
+
+// app.route('/').post( (req, res) => res.send(req.body) );
